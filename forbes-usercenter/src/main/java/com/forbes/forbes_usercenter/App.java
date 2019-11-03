bb1d8103f8f674cda3f42c039a60a79c5efc0cb3
huanghy <hhy131@126.com>
2019-11-03 20:06:49 +0800

    '福布斯用户中心'

---
+++ forbes-usercenter/src/main/java/com/forbes/forbes_usercenter/App.java
@@ -0,0 +1,13 @@
+package com.forbes.forbes_usercenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
