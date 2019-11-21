382d8dfc05e83f08ea7f302d4174b377d797da2a
xfx <1164021132@qq.com>
2019-11-21 18:16:50 +0800

    用户，角色部分接口

---
+++ forbes-usercenter/usercenter-comm/src/main/java/org/forbes/comm/dto/AddUserRoleDto.java
@@ -0,0 +1,19 @@
+package org.forbes.comm.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName
+ * @Description TODO
+ * @Author
+ * @Date 2019/11/21 18:10
+ * @Version 1.0
+ **/
+@Data
+@ApiModel("添加用户角色中间表")
+public class AddUserRoleDto implements Serializable{
+
+}
