66353754aa9a84d0f43800172166b76d992c89a6
cfmkaifa <cfmkaifa@shdbr.com>
2019-11-27 10:30:47 +0800

    sql和vo修改

--- forbes-usercenter/usercenter-comm/src/main/java/org/forbes/comm/vo/SysRolePermissionVo.java
+++ forbes-usercenter/usercenter-comm/src/main/java/org/forbes/comm/vo/SysRolePermissionVo.java
@@ -31,42 +31,6 @@ public class SysRolePermissionVo implements Serializable {
     @ApiModelProperty("权限id")
     private Long id;
 
-
-    /**
-     * 创建人
-     */
-    @ApiModelProperty("权限创建人")
-    private String createBy;
-
-    /**
-     * 创建时间
-     */
-    @JSONField(format="yyyy-MM-dd")
-    @JsonFormat(pattern="yyyy-MM-dd")
-    @ApiModelProperty("权限创建时间")
-    private Date createTime;
-
-    /**
-     * 更新人
-     */
-    @ApiModelProperty("权限id更新人")
-    private String updateBy;
-
-    /**
-     * 更新时间
-     */
-    @ApiModelProperty("权限更新时间")
-    private Date updateTime;
-
-    /**
-     * 父级id
-     * Table:     f_sys_permission
-     * Column:    parent_id
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "父级id")
-    private Integer parentId;
-
     /**
      * 资源名称
      * Table:     f_sys_permission
@@ -75,76 +39,6 @@ public class SysRolePermissionVo implements Serializable {
      */
     @ApiModelProperty(value = "资源名称")
     private String name;
-
-    /**
-     * 菜单权限编码
-     * Table:     f_sys_permission
-     * Column:    perms
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "菜单权限编码")
-    private String perms;
-
-    /**
-     * 组件
-     * Table:     f_sys_permission
-     * Column:    component
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "组件")
-    private String component;
-
-    /**
-     * 组件名称
-     * Table:     f_sys_permission
-     * Column:    component_name
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "组件名称")
-    private String componentName;
-
-    /**
-     * 是否聚合子路由
-     * Table:     f_sys_permission
-     * Column:    always_show
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "是否聚合子路由")
-    private String alwaysShow;
-
-    /**
-     * 是否路由
-     * Table:     f_sys_permission
-     * Column:    is_route
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "是否路由")
-    private String isRoute;
-
-    /**
-     * Table:     f_sys_permission
-     * Column:    is_leaf
-     * Nullable:  true
-     */
-    @ApiModelProperty()
-    private String isLeaf;
-
-    /**
-     * 是否隐藏
-     * Table:     f_sys_permission
-     * Column:    is_hidden
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "是否隐藏")
-    private String isHidden;
-
-    /**
-     * Table:     f_sys_permission
-     * Column:    sort_no
-     * Nullable:  true
-     */
-    @ApiModelProperty()
-    private Integer sortNo;
 
     /**
      * 描述
@@ -156,77 +50,6 @@ public class SysRolePermissionVo implements Serializable {
     private String description;
 
     /**
-     * 路由请求地址
-     * Table:     f_sys_permission
-     * Column:    redirect
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "路由请求地址")
-    private String redirect;
-
-    /**
-     * 后台请求地址
-     * Table:     f_sys_permission
-     * Column:    url
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "后台请求地址")
-    private String url;
-
-    /**
-     * 菜单图标
-     * Table:     f_sys_permission
-     * Column:    icon
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "菜单图标")
-    private byte[] icon;
-
-
-
-
-
-
-
-
-
-    /**
-     * id
-     */
-    @TableId(type = IdType.AUTO)
-    @JSONField(format="yyyy-MM-dd")
-    @JsonFormat(pattern="yyyy-MM-dd")
-    @ApiModelProperty("角色id")
-    private Long role_id;
-
-
-    /**
-     * 创建人
-     */
-    @ApiModelProperty("角色创建人")
-    private String role_createBy;
-
-    /**
-     * 更新人
-     */
-    @JSONField(format="yyyy-MM-dd")
-    @JsonFormat(pattern="yyyy-MM-dd")
-    @ApiModelProperty("角色更新人")
-    private Date role_createTime;
-
-    /**
-     * 更新人
-     */
-    @ApiModelProperty("角色更新人")
-    private String role_updateBy;
-
-    /**
-     * 更新时间
-     */
-    @ApiModelProperty("角色更新时间")
-    private Date role_updateTime;
-
-    /**
      * 角色名称
      * Table:     f_sys_role
      * Column:    role_name
@@ -235,21 +58,4 @@ public class SysRolePermissionVo implements Serializable {
     @ApiModelProperty(value = "角色名称")
     private String roleName;
 
-    /**
-     * 角色编码
-     * Table:     f_sys_role
-     * Column:    role_code
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "角色编码")
-    private String roleCode;
-
-    /**
-     * 角色描述
-     * Table:     f_sys_role
-     * Column:    description
-     * Nullable:  true
-     */
-    @ApiModelProperty(value = "角色描述")
-    private String role_description;
 }
